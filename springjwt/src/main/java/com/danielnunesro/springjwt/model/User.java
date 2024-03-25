06681e790e17e9a6e7bc6edebe44ec0fe731aec2
Daniel Nunes Rodrigues <100057101+danielNunesRo@users.noreply.github.com>
2024-03-25 14:28:37 -0300

    Adicionando a logica no db, entidades criadas

---
+++ springjwt/src/main/java/com/danielnunesro/springjwt/model/User.java
@@ -0,0 +1,196 @@
+package com.danielnunesro.springjwt.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User implements UserDetails, Serializable {
+	
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "user_name", unique = true)
+	private String username;
+	
+	@Column(name = "full_name")
+	private String fullname;
+	
+	@Column(name = "password")
+	private String password;
+	
+	@Column(name = "account_non_expired")
+	private Boolean accountNonExpired;
+	
+	@Column(name = "account_non_locked")
+	private Boolean accountNonLocked;
+	
+	@Column(name = "credentials_non_expired")
+	private Boolean credentials_non_expired;
+	
+	@Column(name = "enabled")
+	private Boolean enabled;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	@JoinTable(name = "user_permission", joinColumns = {@JoinColumn(name = "id_user")}, inverseJoinColumns = {@JoinColumn (name = "id_permission")})
+	private List<Permission> permissions;
+	
+	public User() {
+		
+	}
+	
+	public List<String> getRoles() {
+		
+		List<String> roles = new ArrayList<>();
+		for(Permission permission : permissions) {
+			roles.add(permission.getDescription());
+		}
+		
+		return roles ;
+		
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.permissions;
+	}
+
+	@Override
+	public String getPassword() {
+		return this.password;
+	}
+
+	@Override
+	public String getUsername() {
+		return this.username;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return this.isAccountNonExpired();
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return this.isAccountNonLocked();
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return this.isCredentialsNonExpired();
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return this.isEnabled();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getFullname() {
+		return fullname;
+	}
+
+	public void setFullname(String fullname) {
+		this.fullname = fullname;
+	}
+
+	public Boolean getAccountNonExpired() {
+		return accountNonExpired;
+	}
+
+	public void setAccountNonExpired(Boolean accountNonExpired) {
+		this.accountNonExpired = accountNonExpired;
+	}
+
+	public Boolean getAccountNonLocked() {
+		return accountNonLocked;
+	}
+
+	public void setAccountNonLocked(Boolean accountNonLocked) {
+		this.accountNonLocked = accountNonLocked;
+	}
+
+	public Boolean getCredentials_non_expired() {
+		return credentials_non_expired;
+	}
+
+	public void setCredentials_non_expired(Boolean credentials_non_expired) {
+		this.credentials_non_expired = credentials_non_expired;
+	}
+
+	public Boolean getEnabled() {
+		return enabled;
+	}
+
+	public void setEnabled(Boolean enabled) {
+		this.enabled = enabled;
+	}
+
+	public List<Permission> getPermissions() {
+		return permissions;
+	}
+
+	public void setPermissions(List<Permission> permissions) {
+		this.permissions = permissions;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(accountNonExpired, accountNonLocked, credentials_non_expired, enabled, fullname, id,
+				password, permissions, username);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		return Objects.equals(accountNonExpired, other.accountNonExpired)
+				&& Objects.equals(accountNonLocked, other.accountNonLocked)
+				&& Objects.equals(credentials_non_expired, other.credentials_non_expired)
+				&& Objects.equals(enabled, other.enabled) && Objects.equals(fullname, other.fullname)
+				&& Objects.equals(id, other.id) && Objects.equals(password, other.password)
+				&& Objects.equals(permissions, other.permissions) && Objects.equals(username, other.username);
+	}
+	
+	
+}
