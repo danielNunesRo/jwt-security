2524ca1b3cd58b9f9d5432c986ff76d39ad22ed6
Daniel Nunes Rodrigues <100057101+danielNunesRo@users.noreply.github.com>
2024-03-25 16:38:36 -0300

    Implementando o JwtConfigure e JwtTokenFilter

--- springjwt/src/main/java/com/danielnunesro/springjwt/security/jwt/JwtTokenProvider.java
+++ springjwt/src/main/java/com/danielnunesro/springjwt/security/jwt/JwtTokenProvider.java
@@ -30,7 +30,7 @@ public class JwtTokenProvider {
 	@Value("${security.jwt.token.secret-key:secret}")
 	private String secretKey = "secret";
 	
-	@Value("${security.jwt.token.expire-lenght:3600000}")
+	@Value("${security.jwt.token.expire-length:3600000}")
 	private long validityInMillisecons = 3600000;
 	
 	@Autowired
