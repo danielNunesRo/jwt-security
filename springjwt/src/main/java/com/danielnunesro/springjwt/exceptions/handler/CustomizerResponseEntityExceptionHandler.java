10d758103bda78f57f2c96f9199444f682af8e4f
Daniel Nunes Rodrigues <100057101+danielNunesRo@users.noreply.github.com>
2024-03-25 15:04:13 -0300

    Add exceptions personalizadas, add os VO

---
+++ springjwt/src/main/java/com/danielnunesro/springjwt/exceptions/handler/CustomizerResponseEntityExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.danielnunesro.springjwt.exceptions.handler;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.WebRequest;
+
+import com.danielnunesro.springjwt.exceptions.ExceptionResponse;
+import com.danielnunesro.springjwt.exceptions.InvalidJwtAuthenticationException;
+
+@RestController
+@ControllerAdvice
+public class CustomizerResponseEntityExceptionHandler {
+	
+	@ExceptionHandler(InvalidJwtAuthenticationException.class)
+	public final ResponseEntity<ExceptionResponse> handleInvalidJwtAuthentication(Exception ex, WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.FORBIDDEN);
+	}
+	
+}
